b4f58d93cd4055831bb9f0f63a118fb06b648335
Jonas Juselius <jonas.juselius@uit.no>
2014-12-02 19:32:29 +0100

    Natural key actions.

--- snake.hs
+++ snake.hs
@@ -5,18 +5,19 @@ import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 import Reactive.Threepenny
 
+data Move = U | D | L | R
+
+data Snake = Snake {
+      trunk :: [(Double, Double)]
+    , heading :: Move
+    }
+
 main :: IO ()
 main = startGUI defaultConfig setup
 
 width, height :: Int
 width = 500
 height = 400
-
-data Direction = U | D | L | R
-data Snake = Snake {
-      segment :: [(Double, Double)]
-    , dir :: Direction
-    }
 
 newSnake :: Snake
 newSnake = Snake [(10.0 * x, 100.0) | x <- [9,8..1]] R
@@ -34,17 +35,15 @@ setup window = void $ do
     restart <- UI.button # set text "Restart"
     t <- liftIO $ newIORef (0::Int)
     snake <- liftIO $ newIORef newSnake
-    foo <- string "0"
+    curtime <- string "0"
     keycode <- string "n/a"
     bdy <- getBody window
     getBody window #+ [column [
           element canvas
         , row [element start, element stop, element restart]
-        , row [element foo]
-        , row [element keycode]
+        , row [element curtime]
         ]]
-
-    drawSnake "green" canvas newSnake
+    drawSnake "green" canvas snake
 
     on UI.click start   . const $ UI.start timer
     on UI.click stop    . const $ UI.stop timer
@@ -52,51 +51,79 @@ setup window = void $ do
         UI.clearCanvas canvas
         liftIO $ writeIORef t 0
         liftIO $ writeIORef snake newSnake
-        drawSnake "green" canvas newSnake
+        drawSnake "green" canvas snake
 
-    on UI.keydown bdy $ \k -> liftIO $ do
-        s <- readIORef snake
-        writeIORef snake $ s { dir =
-            case k of
-                37 -> L
-                38 -> U
-                39 -> R
-                40 -> D
-                _ -> dir s
-            }
-
-    on UI.keydown bdy $ \k -> element keycode # set text (show k)
+    on UI.keydown bdy $ \k -> do
+        running <- get UI.running timer
+        if running
+            then do
+                liftIO $ setHeading snake k
+                if isMove k
+                    then updateSnake canvas t snake
+                    else return ()
+            else return ()
 
     on UI.tick timer $ const $ do
-        liftIO $ modifyIORef t (+1)
+        updateSnake canvas t snake
         t' <- liftIO $ readIORef t
-        element foo # set text (show t')
-        delTail snake canvas
-        liftIO $ modifyIORef snake $ moveSnake
-        snake' <- liftIO $ readIORef snake
-        drawSnake "green" canvas snake'
+        element curtime # set text (show t')
+
+updateSnake :: Element -> IORef Int -> IORef Snake -> UI ()
+updateSnake canvas time snake = do
+        liftIO $ modifyIORef time (+1)
+        t <- liftIO $ readIORef time
+        moveSnake canvas snake
 
 delTail :: IORef Snake -> Element -> UI ()
 delTail s canvas = do
     s' <- liftIO $ readIORef s
-    let h = last $ segment s'
-        snake = s' { segment = init (segment s') }
+    let h = last $ trunk s'
+        snake = s' { trunk = init (trunk s') }
     liftIO $ writeIORef s snake
     element canvas # set UI.fillStyle (UI.htmlColor "white")
     UI.fillRect h 10 10 canvas
 
-moveSnake :: Snake -> Snake
-moveSnake s@(Snake b d) = case d of
-    U -> s { segment = (x, y - tt) : b }
-    D -> s { segment = (x, y + tt) : b }
-    R -> s { segment = (x + tt, y) : b }
-    L -> s { segment = (x - tt, y) : b }
+moveSnake :: Element -> IORef Snake -> UI ()
+moveSnake canvas snake = do
+    delTail snake canvas
+    chSnake
+    drawSnake "green" canvas snake
     where
-        (x, y) = head b
-        tt = 10.0
+        chSnake = liftIO $ do
+            s' <- readIORef snake
+            let s@(Snake b d) = s'
+                (x, y) = head b
+            writeIORef snake $
+                case d of
+                    U -> s { trunk = (x, y - tt) : b }
+                    D -> s { trunk = (x, y + tt) : b }
+                    R -> s { trunk = (x + tt, y) : b }
+                    L -> s { trunk = (x - tt, y) : b }
+                    where
+                        tt = 10.0
 
-drawSnake :: String -> Element -> Snake -> UI ()
+drawSnake :: String -> Element -> IORef Snake -> UI ()
 drawSnake color canvas snake = do
+    s <- liftIO $ readIORef snake
     element canvas # set UI.fillStyle (UI.htmlColor color)
-    mapM_ (\h -> UI.fillRect h 10 10 canvas) (segment snake)
+    mapM_ (\h -> UI.fillRect h 10 10 canvas) (trunk s)
+
+isMove :: Int -> Bool
+isMove k = case k of
+        37 -> True
+        38 -> True
+        39 -> True
+        40 -> True
+        _ -> False
+
+setHeading :: IORef Snake -> Int -> IO ()
+setHeading snake k = do
+    s <- readIORef snake
+    writeIORef snake $ s { heading = case k of
+        37 -> L
+        38 -> U
+        39 -> R
+        40 -> D
+        _ -> heading s
+        }
 
