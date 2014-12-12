a19362ee9d035e39e9942451b0f2ffc498c83af4
Jonas Juselius <jonas.juselius@uit.no>
2014-12-12 08:32:05 +0100

    Made examples easier to read.

--- frp_short0.hs
+++ frp_short0.hs
@@ -10,10 +10,13 @@ main = startGUI defaultConfig setup
 setup :: Window -> UI ()
 setup _ = void $ do
     (e0, fire) <- liftIO  UI.newEvent
-
-    let e0' = (\w -> w { intW = succ $ intW w
-        , runW = liftIO . print $ intW w }) <$ e0
+    let e0' = action <$ e0
     e <- accumE (World 0 (return ())) e0'
     onEvent e $ \w -> void $ runW w
     replicateM_ 5 . liftIO $ fire ()
+    where
+        action :: World -> World
+        action w = w { intW = succ $ intW w
+                     , runW = liftIO . print $ intW w
+                     }
 
--- frp_short1.hs
+++ frp_short1.hs
@@ -11,13 +11,15 @@ setup :: Window -> UI ()
 setup _ = void $ do
     (e0, fire) <- liftIO UI.newEvent
 
-    let e0' = (\world -> do
-        w <- world
-        let w' = w { intW = succ $ intW w }
-        liftIO $ print w'
-        return w'
-        ) <$ e0
+    let e0' = action <$ e0
     e <- accumE (return (World 0)) e0'
     onEvent e void
     replicateM_ 5 . liftIO $ fire ()
+    where
+        action :: UI World -> UI World
+        action world = do
+            w <- world
+            let w' = w { intW = succ $ intW w }
+            liftIO $ print w'
+            return w'
 
