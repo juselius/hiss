35b3ac456692c52db74ef0b6a549e8c0b81c9451
Jonas Juselius <jonas.juselius@uit.no>
2014-12-15 14:53:57 +0100

    FRP test: Timer with reset.

--- sandbox/frp_test2.hs
+++ sandbox/frp_test2.hs
@@ -7,16 +7,9 @@ import Graphics.UI.Threepenny.Core
 main :: IO ()
 main = startGUI defaultConfig setup
 
-data World = World {
-      strW :: String
-    , timeW :: Int
-    }
-
 data ActiveElements = AE {
       btn0   :: Element
     , btn1   :: Element
-    , btn2   :: Element
-    , infos  :: Element
     , tStr0  :: Element
     , tStr1  :: Element
     }
@@ -25,46 +18,44 @@ activeElements :: UI ActiveElements
 activeElements = do
     btn0  <- UI.button # set text "Start"
     btn1  <- UI.button # set text "Stop"
-    btn2  <- UI.button # set text "More!"
-    infos <- string "waiting..."
     tStr0 <- string "0"
     tStr1 <- string "0"
     return AE {
           btn0   = btn0
         , btn1   = btn1
-        , btn2   = btn2
-        , infos  = infos
         , tStr0  = tStr0
         , tStr1  = tStr1
         }
 
 setup :: Window -> UI ()
 setup window = void $ do
-    return window # set title "FRP Test 1"
-    (AE {..}) <- activeElements
-    let wrl = World "" 0
+    void $ return window # set title "FRP Test 1"
 
-    _ <- getBody window #+ [
-          row [element btn0, element btn1, element btn2]
-        , row [string "stat = ", element infos]
+    (AE {..}) <- activeElements
+    void $ getBody window #+ [
+          row [element btn0, element btn1]
         , row [string "timer = ", element tStr0]
         , row [string "ticks = ", element tStr1 ]
         ]
+
     tx <- UI.timer
-    let eTick = UI.tick tx
+    let eTick = (+1) <$ UI.tick tx
+        eStop = const (0::Int) <$ UI.click btn1
+    bTimer0 <- accumB (0::Int) ((+1) <$ eTick)
+    bTimer1 <- accumB (0::Int) $ head <$> unions [eStop,  eTick]
+
     on UI.click btn0 $ const (UI.start tx)
     on UI.click btn1 $ const (UI.stop tx)
-    let bStop = pure (0) <@ UI.click btn1
-    bTimer <- accumB (0::Int) ((+1) <$ eTick)
-    eTimer <- accumE (0::Int) ((+1) <$ eTick)
-    let eTimer' = bStop <@ eTimer
-    bTimer' <- stepper (0::Int) eTimer'
-    element tStr1 # sink text (show <$> bTimer)
-    element tStr0 # sink text (show <$> bTimer')
-    onChanges bTimer $ \w -> runW w
-    --onChanges bStop $ \w -> liftIO $ print (w 1)
 
+    onChanges bTimer0 $ \w -> runW bTimer1 w
+    onChanges bTimer1 $ \w -> liftIO $ print w
 
-runW :: Int -> UI ()
-runW t = liftIO $ putStrLn $ "t = " ++ show t
+    void $ element tStr1 # sink text (show <$> bTimer0)
+    void $ element tStr0 # sink text (show <$> bTimer1)
+
+runW :: Behavior Int -> Int -> UI ()
+runW b t = do
+    b' <- currentValue b
+    liftIO $ putStr $ "t = " ++ show t
+    liftIO $ putStrLn $ ", b = " ++ show b'
 
